49e3d6c9c50e4639684061716e6c10429a9f4feb
falsycat <me@falsy.cat>
2022-11-10 22:40:24 +0900

    add Font/Context to default root

--- init.hh
+++ init.hh
@@ -10,6 +10,7 @@ inline std::unique_ptr<nf7::File> CreateRoot(nf7::Env& env) noexcept {
   auto& dir = ret->interfaceOrThrow<nf7::Dir>();
 
   dir.Add("_audio", nf7::File::registry("Audio/Context").Create(env));
+  dir.Add("_font", nf7::File::registry("Font/Context").Create(env));
   dir.Add("_imgui", nf7::File::registry("System/ImGui").Create(env));
   dir.Add("_logger", nf7::File::registry("System/Logger").Create(env));
   dir.Add("_luajit", nf7::File::registry("LuaJIT/Context").Create(env));
