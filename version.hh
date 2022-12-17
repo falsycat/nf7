c8feba19c0894141cb7054c1ef374667cf5779bc
falsycat <me@falsy.cat>
2022-12-17 12:15:31 +0900

    update version number

--- version.hh
+++ version.hh
@@ -5,9 +5,9 @@ namespace nf7 {
 struct Version {
   static constexpr size_t kMajor = 0;
   static constexpr size_t kMinor = 4;
-  static constexpr size_t kPatch = 0;
+  static constexpr size_t kPatch = 1;
 
-  static constexpr const char* kString = "v0.4.0";
+  static constexpr const char* kString = "v0.4.1";
 };
 
 }  // namespace nf7
