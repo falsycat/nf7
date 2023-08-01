eafa05fbf5d6f9a1926d1553240598feebf159dd
falsycat <me@falsy.cat>
2023-08-01 21:01:16 +0900

    remove fields from DealerMeta
    they are unnecessary because lambda no longer has multiple takers/makers

--- iface/common/dealer_test.cc
+++ iface/common/dealer_test.cc
@@ -7,7 +7,7 @@
 
 
 TEST(Emitter, Emit) {
-  nf7::Emitter<int32_t> sut {{"hello", "this is a maker"}};
+  nf7::Emitter<int32_t> sut {};
   nf7::test::ObserverMock<int32_t> obs {sut};
 
   EXPECT_CALL(obs, NotifyWithMove(int32_t {777})).Times(1);
