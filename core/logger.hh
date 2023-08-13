32fc72824b077fedc1ddfd2fc9d4f4b72986d718
falsycat <me@falsy.cat>
2023-08-13 12:11:29 +0900

    add NullLogger

---
+++ core/logger.hh
@@ -0,0 +1,29 @@
+// No copyright
+#pragma once
+
+#include <memory>
+
+#include "iface/common/exception.hh"
+#include "iface/subsys/logger.hh"
+
+
+namespace nf7::core {
+
+class NullLogger : public subsys::Logger {
+ public:
+  static const std::shared_ptr<NullLogger>& instance()
+  try {
+    static const auto kInstance = std::make_shared<NullLogger>();
+    return kInstance;
+  } catch (const std::bad_alloc&) {
+    throw Exception {"memory shortage"};
+  }
+
+ public:
+  NullLogger() noexcept : subsys::Logger("nf7::core::NullLogger") { }
+
+ public:
+  void Push(const Item&) noexcept override { }
+};
+
+}  // namespace nf7::core
