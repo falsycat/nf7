08154d127ac178640464dd64a5f1a2a15b74eb2b
falsycat <me@falsy.cat>
2023-08-16 18:46:35 +0900

    add exception property to logger item

--- iface/subsys/logger.hh
+++ iface/subsys/logger.hh
@@ -1,6 +1,7 @@
 // No copyright
 #pragma once
 
+#include <exception>
 #include <source_location>
 #include <string>
 #include <string_view>
@@ -26,8 +27,12 @@ class Logger : public Interface {
     Item() = delete;
     Item(Level level,
          std::string_view contents,
-         std::source_location srcloc = std::source_location::current()) noexcept
-        : level_(level), contents_(contents), srcloc_(srcloc) { }
+         std::source_location srcloc = std::source_location::current(),
+         std::exception_ptr ex = std::current_exception()) noexcept
+        : level_(level),
+          contents_(contents),
+          srcloc_(srcloc),
+          exception_(ex) { }
 
     Item(const Item&) = default;
     Item(Item&&) = default;
@@ -37,11 +42,13 @@ class Logger : public Interface {
     Level level() const noexcept { return level_; }
     const std::string& contents() const noexcept { return contents_; }
     const std::source_location& srcloc() const noexcept { return srcloc_; }
+    const std::exception_ptr& exception() const noexcept { return exception_; }
 
    private:
     Level level_;
     std::string contents_;
     std::source_location srcloc_;
+    std::exception_ptr exception_;
   };
 
  public:
@@ -53,28 +60,31 @@ class Logger : public Interface {
 
   // THREAD-SAFE
   void Trace(std::string_view contents,
-             SrcLoc srcloc = SrcLoc::current()) noexcept {
-    Push(Item {kTrace, contents, srcloc});
+             SrcLoc srcloc = SrcLoc::current(),
+             std::exception_ptr ex = std::current_exception()) noexcept {
+    Push(Item {kTrace, contents, srcloc, ex});
   }
 
   // THREAD-SAFE
   void Info(std::string_view contents,
-            SrcLoc srcloc = SrcLoc::current()) noexcept {
-    Push(Item {kInfo, contents, srcloc});
+            SrcLoc srcloc = SrcLoc::current(),
+             std::exception_ptr ex = std::current_exception()) noexcept {
+    Push(Item {kInfo, contents, srcloc, ex});
   }
 
   // THREAD-SAFE
   void Warn(std::string_view contents,
-            SrcLoc srcloc = SrcLoc::current()) noexcept {
-    Push(Item {kWarn, contents, srcloc});
+            SrcLoc srcloc = SrcLoc::current(),
+             std::exception_ptr ex = std::current_exception()) noexcept {
+    Push(Item {kWarn, contents, srcloc, ex});
   }
 
   // THREAD-SAFE
   void Error(std::string_view contents,
-             SrcLoc srcloc = SrcLoc::current()) noexcept {
-    Push(Item {kError, contents, srcloc});
+             SrcLoc srcloc = SrcLoc::current(),
+             std::exception_ptr ex = std::current_exception()) noexcept {
+    Push(Item {kError, contents, srcloc, ex});
   }
 };
-
 
 }  // namespace nf7::subsys
