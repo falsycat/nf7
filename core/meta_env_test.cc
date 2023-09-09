efe922f835bafced06199c123be24033d071f466
falsycat <me@falsy.cat>
2023-09-09 09:21:17 +0900

    add MetaEnv implementation

---
+++ core/meta_env_test.cc
@@ -0,0 +1,74 @@
+// No copyright
+#include "core/meta_env.hh"
+
+#include <gtest/gtest.h>
+
+#include "iface/env.hh"
+
+
+static inline std::shared_ptr<nf7::Env> MakeEmptyEnv() {
+  return std::make_shared<nf7::SimpleEnv>(nf7::SimpleEnv::FactoryMap {});
+}
+static inline bool MatchPair(const std::optional<nf7::subsys::MetaEnv::Pair>& a,
+                             const nf7::subsys::MetaEnv::Pair& b) {
+  return a && a->first == b.first && &a->second == &b.second;
+}
+
+TEST(MetaEnv, FindOrByName) {
+  const auto a = MakeEmptyEnv();
+  const auto b = MakeEmptyEnv();
+  const auto c = MakeEmptyEnv();
+
+  nf7::core::MetaEnv sut {
+    {
+      {"b", b},
+      {"c", c},
+      {"a", a},
+    },
+  };
+
+  EXPECT_EQ(sut.FindOr(""), nullptr);
+  EXPECT_EQ(sut.FindOr("a"), a.get());
+  EXPECT_EQ(sut.FindOr("b"), b.get());
+  EXPECT_EQ(sut.FindOr("c"), c.get());
+  EXPECT_EQ(sut.FindOr("d"), nullptr);
+}
+
+TEST(MetaEnv, FindOrByIndex) {
+  const auto a = MakeEmptyEnv();
+  const auto b = MakeEmptyEnv();
+  const auto c = MakeEmptyEnv();
+
+  nf7::core::MetaEnv sut {
+    {
+      {"b", b},
+      {"c", c},
+      {"a", a},
+    },
+  };
+
+  EXPECT_TRUE(MatchPair(sut.FindOr(0), {"a", *a}));
+  EXPECT_TRUE(MatchPair(sut.FindOr(1), {"b", *b}));
+  EXPECT_TRUE(MatchPair(sut.FindOr(2), {"c", *c}));
+  EXPECT_EQ(sut.FindOr(3), std::nullopt);
+}
+
+TEST(MetaEnv, FetchAll) {
+  const auto a = MakeEmptyEnv();
+  const auto b = MakeEmptyEnv();
+  const auto c = MakeEmptyEnv();
+
+  nf7::core::MetaEnv sut {
+    {
+      {"b", b},
+      {"c", c},
+      {"a", a},
+    },
+  };
+
+  const auto all = sut.FetchAll();
+  EXPECT_EQ(all.size(), 3);
+  EXPECT_TRUE(MatchPair(all[0], {"a", *a}));
+  EXPECT_TRUE(MatchPair(all[1], {"b", *b}));
+  EXPECT_TRUE(MatchPair(all[2], {"c", *c}));
+}
