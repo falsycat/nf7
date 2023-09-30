74f740d87e04521424baf7c03abe0927851321f3
falsycat <me@falsy.cat>
2023-09-30 14:30:44 +0900

    add ContextFixture for testing with imgui context

--- core/imgui/context_test.cc
+++ core/imgui/context_test.cc
@@ -7,18 +7,18 @@
 
 #include <chrono>
 
-#include "core/gl3/context_test.hh"
+#include "core/imgui/context_test.hh"
 #include "core/imgui/driver_test.hh"
 
 
 // adds meaningless suffix to avoid a conflict with ImGuiContext
-using ImGuiContext0 = nf7::core::gl3::test::ContextFixture;
+using ImGuiContext0 = nf7::core::imgui::test::ContextFixture;
 
 TEST_F(ImGuiContext0, Init) {
   const auto clock       = env().Get<nf7::subsys::Clock>();
   const auto concurrency = env().Get<nf7::subsys::Concurrency>();
 
-  auto ctx = std::make_shared<nf7::core::imgui::Context>(env());
+  auto ctx = env().Get<nf7::core::imgui::Context>();
   concurrency->Push(nf7::SyncTask {
     clock->now() + std::chrono::seconds {10},
     [&](auto&) { ctx = nullptr; },
