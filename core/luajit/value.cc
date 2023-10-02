2108ca3210e42219880d18802ce74bb226a0a944
falsycat <me@falsy.cat>
2023-10-02 23:25:34 +0900

    add luajit::Value::MakeFunction

--- core/luajit/value.cc
+++ core/luajit/value.cc
@@ -1,15 +1,50 @@
 // No copyright
 #include "core/luajit/value.hh"
 
+#include <utility>
+
+#include "iface/common/exception.hh"
+#include "iface/subsys/concurrency.hh"
+
 #include "core/luajit/context.hh"
 
 
 namespace nf7::core::luajit {
 
+std::shared_ptr<Value> Value::MakeFunction(
+    TaskContext& ctx, std::span<const uint8_t> buf, const char* name) {
+  const auto ret = luaL_loadbuffer(
+      *ctx, reinterpret_cast<const char*>(buf.data()), buf.size(), name);
+  switch (ret) {
+  case LUA_OK:
+    return ctx.Register();
+  case LUA_ERRMEM:
+    lua_pop(*ctx, 1);
+    throw MemoryException {};
+  default:
+    std::string msg {lua_tostring(*ctx, -1)};
+    lua_pop(*ctx, 1);
+    throw Exception {"failed to compile a buffer" + msg};
+  }
+}
+Future<std::shared_ptr<Value>> Value::MakeFunction(
+    Env& env,
+    std::vector<uint8_t>&& v,
+    std::string name) noexcept {
+  return Future<std::shared_ptr<Value>>::Completer {}
+      .RunAsync(
+          env.Get<Context>(),
+          env.Get<subsys::Concurrency>(),
+          [v = std::move(v), name = std::move(name)](auto& ctx) {
+            return MakeFunction(ctx, v, name.c_str());
+          })
+      .future();
+}
+
 Value::~Value() noexcept {
-  ctx_->Push(Task {[index = index_](auto& ctx) {
+  ctx_->Exec([index = index_](auto& ctx) {
     luaL_unref(*ctx, LUA_REGISTRYINDEX, index);
-  }});
+  });
 }
 
 }  // namespace nf7::core::luajit
--- core/luajit/value.hh
+++ core/luajit/value.hh
@@ -2,16 +2,34 @@
 #pragma once
 
 #include <cassert>
+#include <cstdint>
 #include <memory>
+#include <span>
+#include <string>
+#include <string_view>
+#include <vector>
 
 #include "iface/common/value.hh"
+#include "iface/common/future.hh"
+#include "iface/env.hh"
 
 
 namespace nf7::core::luajit {
 
 class Context;
+class TaskContext;
 
 class Value final : public nf7::Value::Data {
+ public:
+  static std::shared_ptr<Value> MakeFunction(
+      TaskContext&,
+      std::span<const uint8_t>,
+      const char* name = "");
+  static Future<std::shared_ptr<Value>> MakeFunction(
+      Env&,
+      std::vector<uint8_t>&&,
+      std::string name = "") noexcept;
+
  public:
   Value() = delete;
   Value(const std::shared_ptr<Context>& ctx, int index) noexcept
