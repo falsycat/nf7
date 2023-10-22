7a5e51d94856e6a831dce73f54025c67fed0cf09
falsycat <me@falsy.cat>
2023-10-22 11:29:24 +0900

    support overloaded imgui functions

--- cmake/imgui4lua.py
+++ cmake/imgui4lua.py
@@ -18,16 +18,50 @@ def gen_enum(item):
     print(f"  lua_pushinteger(L, static_cast<lua_Integer>({member_name})); "+
           f"lua_setfield(L, -2, \"{short_name}_{member_short_name}\");")
   print(f"}}  // {name}")
+  print()
 
-def gen_func(item):
-  name = item["name"]
-  if name not in kFuncWhitelist:
-    return
+def gen_func(name, ovlds):
+  ovld_root = (0, {}, None)
+  for ovld in ovlds:
+    ovld_node = ovld_root
+    idx       = 0
+    for arg in ovld.get("inner", []):
+      if "ParmVarDecl" != arg.get("kind"): continue
+      tname = get_type_from_argument(arg)
+      if tname not in kLuaTypeChecker:
+        break
+      ovld_node[1][tname] = (idx, {}, None)
+      ovld_node = ovld_node[1][tname]
+      idx += 1
+    ovld_node[1]["$"] = (idx, {}, ovld)
 
+  def _output(ovld_node, depth=1):
+    indent = "  "*depth
+    if 0 == len(ovld_node[1]):
+      gen_single_func(ovld_node[2], indent=indent)
+    elif 1 == len(ovld_node[1]):
+      _output(*ovld_node[1].values(), depth=depth)
+    else:
+      for tname in ovld_node[1]:
+        checker = kLuaTypeChecker[tname].replace("#", str(ovld_node[0]+1))
+        print(f"{indent}if ({checker}) {{")
+        _output(ovld_node[1][tname], depth=depth+1)
+        print(f"{indent}}}")
+      print(f"{indent}return luaL_error(L, \"unexpected type in param#{ovld_node[0]}\");")
+  print("lua_pushcfunction(L, [](auto L) {")
+  print("  (void) L;")
+  _output(ovld_root)
+  print("});")
+  print(f"lua_setfield(L, -2, \"{name}\");")
+  print()
+
+def gen_single_func(item, indent=""):
+  name   = item["name"]
   pops   = []
   params = []
   pcount = 0
   for arg in item.get("inner", []):
+    if "ParmVarDecl" != arg.get("kind"): continue
     pop, param, push = gen_argument(pcount, arg)
     pcount += 1
     pops  .extend(pop)
@@ -38,24 +72,17 @@ def gen_func(item):
   use_ret = 0 < len(pushes)
 
   # text output
-  nl = "\n  "
+  nl = "\n"+indent
   cm = ", "
-  if 0 < len(pops) or 0 < len(pushes):
-    print("lua_pushcfunction(L, [](auto L) {")
-  else:
-    print("lua_pushcfunction(L, [](auto) {")
   if 0 < len(pops):
-    print(f"  {nl.join(pops)}")
+    print(f"{indent}{nl.join(pops)}")
   if use_ret:
-    print(f"  const auto r = ImGui::{name}({cm.join(params)});")
+    print(f"{indent}const auto r = ImGui::{name}({cm.join(params)});")
   else:
-    print(f"  ImGui::{name}({cm.join(params)});")
+    print(f"{indent}ImGui::{name}({cm.join(params)});")
   if 0 < len(pushes):
-    print(f"  {nl.join(pushes)}")
-  print(f"  return {len(pushes)};")
-  print("});")
-  print(f"lua_setfield(L, -2, \"{name}\");")
-  print()
+    print(f"{indent}{nl.join(pushes)}")
+  print(f"{indent}return {len(pushes)};")
 
 def gen_return(item):
   ftype = item["type"]["qualType"]
@@ -103,26 +130,45 @@ def gen_argument(pc, item):
   return ([], [], [])
 
 
+# ---- GENERATOR UTILITIES
+def get_type_from_argument(item):
+  return item["type"].get("desugaredQualType", item["type"]["qualType"])
+
+
 # ---- WALKER DEFINITIONS
-def walk_ns(item):
-  kind = item.get("kind")
+class Walker:
+  def __init__(self):
+    self._funcs = {}
+    self._enums = {}
 
-  if "FunctionDecl" == kind:
-    gen_func(item)
-  else:
-    walk(item)
+  def emit(self):
+    for x in self._enums: gen_enum(self._enums[x])
+    for x in self._funcs: gen_func(x, self._funcs[x])
 
-def walk(item):
-  kind = item.get("kind")
+  def walk(self, item):
+    kind = item.get("kind")
+    name = item.get("name")
+    if "EnumDecl" == kind:
+      if name in kEnumWhitelist:
+        self._enums[name] = item
+    else:
+      w = self.walk
+      if "NamespaceDecl" == kind:
+        w = self._walk_ns
+      for child in item.get("inner", []):
+        w(child)
 
-  if "EnumDecl" == kind:
-    gen_enum(item)
-  else:
-    w = walk
-    if kind == "NamespaceDecl":
-      w = walk_ns
-    for child in item.get("inner", []):
-      w(child)
+  def _walk_ns(self, item):
+    kind = item.get("kind")
+    name = item.get("name")
+    if "FunctionDecl" == kind:
+      if name in kFuncWhitelist:
+        if name not in self._funcs:
+          self._funcs[name] = [item]
+        else:
+          self._funcs[name].append(item)
+    else:
+      self.walk(item)
 
 
 # ---- DATA DEFINITIONS
@@ -139,16 +185,24 @@ kFuncWhitelist = [
   "GetWindowSize",
   "GetWindowWidth",
   "GetWindowHeight",
+  "Text",
 ]
 kEnumWhitelist = [
   "ImGuiWindowFlags_",
 ]
+kLuaTypeChecker = {
+  "int": "LUA_TNUMBER == lua_type(L, #)",
+  "const char *": "LUA_TSTRING == lua_type(L, #)",
+  "$": "lua_isnone(L, #)",
+}
 
 
 # ---- ENTRYPOINT
 proc = subprocess.run(["clang++", "-x", "c++", "-std=c++2b", "-Xclang", "-ast-dump=json", "-fsyntax-only", sys.argv[1]], capture_output=True)
 if 0 == proc.returncode:
-  walk(json.loads(proc.stdout))
+  walker = Walker()
+  walker.walk(json.loads(proc.stdout))
+  walker.emit()
 else:
   print(proc.stderr.decode("utf-8"))
   exit(1)
