37deb780efbc5d7d44c43f3686b72376b1d971da
falsycat <me@falsy.cat>
2023-12-31 13:17:12 +0900

    colorize log output

---
+++ util/ansi.h
@@ -0,0 +1,9 @@
+// No copyright
+#pragma once
+
+#define NF7UTIL_ANSI_RESET "\x1b[0m"
+#define NF7UTIL_ANSI_BOLD  "\x1b[1m"
+
+#define NF7UTIL_ANSI_BLUE   "\x1b[34m"
+#define NF7UTIL_ANSI_YELLOW "\x1b[33m"
+#define NF7UTIL_ANSI_RED    "\x1b[31m"
--- util/log.h
+++ util/log.h
@@ -11,43 +11,52 @@
 
 #include <uv.h>
 
+#include "util/ansi.h"
 
-#define NF7UTIL_LOG_CURRENT_FILE  \
-    ((const char*) __FILE__ + NF7_PROJECT_DIR_LEN)
+
+#define NF7UTIL_LOG_PREFIX_DEBUG "DBG"
+#define NF7UTIL_LOG_PREFIX_INFO  \
+    NF7UTIL_ANSI_BOLD NF7UTIL_ANSI_BLUE   "INF" NF7UTIL_ANSI_RESET
+#define NF7UTIL_LOG_PREFIX_WARN  \
+    NF7UTIL_ANSI_BOLD NF7UTIL_ANSI_YELLOW "WRN" NF7UTIL_ANSI_RESET
+#define NF7UTIL_LOG_PREFIX_ERROR  \
+    NF7UTIL_ANSI_BOLD NF7UTIL_ANSI_RED    "ERR" NF7UTIL_ANSI_RESET
+
 
 #define nf7util_log(level, file, line, func, fmt, ...)  \
     printf(level "|%s:%" PRIu64 "|%s|" fmt "\n", file, (uint64_t) line, func __VA_OPT__(,) __VA_ARGS__)
 #define nf7util_log_sugar(level, ...)  \
-    nf7util_log(level, NF7UTIL_LOG_CURRENT_FILE, __LINE__, __func__, __VA_ARGS__)
+    nf7util_log(level, __FILE__, __LINE__, __func__, __VA_ARGS__)
 
 #if !defined(NDEBUG)
 # define nf7util_log_debug(...)  \
-    nf7util_log_sugar("DBG", __VA_ARGS__)
+    nf7util_log_sugar(NF7UTIL_LOG_PREFIX_DEBUG, __VA_ARGS__)
 #else
 # define nf7util_log_debug(fmt, ...) do { } while (0)
 #endif
 
 #define nf7util_log_info(...)  \
-    nf7util_log_sugar("INF", __VA_ARGS__)
+    nf7util_log_sugar(NF7UTIL_LOG_PREFIX_INFO, __VA_ARGS__)
 #define nf7util_log_warn(...)  \
-    nf7util_log_sugar("WRN", __VA_ARGS__)
+    nf7util_log_sugar(NF7UTIL_LOG_PREFIX_WARN, __VA_ARGS__)
 #define nf7util_log_error(...)  \
-    nf7util_log_sugar("ERR", __VA_ARGS__)
+    nf7util_log_sugar(NF7UTIL_LOG_PREFIX_ERROR, __VA_ARGS__)
 
 
 // ---- libuv utils
 #define nf7util_log_uv(ret)  \
-    nf7util_log_uv_((ret), NF7UTIL_LOG_CURRENT_FILE, __LINE__, __func__)
+    nf7util_log_uv_((ret), __FILE__, __LINE__, __func__)
 static inline int nf7util_log_uv_(
     int ret, const char* file, uint64_t line, const char* func) {
   if (0 != ret) {
-    nf7util_log("INF", file, line, func, "uv api error: %s", uv_strerror(ret));
+    nf7util_log(NF7UTIL_LOG_PREFIX_INFO,
+                file, line, func, "uv api error: %s", uv_strerror(ret));
   }
   return ret;
 }
 
 #define nf7util_log_uv_assert(ret)  \
-    nf7util_log_uv_((ret), NF7UTIL_LOG_CURRENT_FILE, __LINE__, __func__)
+    nf7util_log_uv_((ret), __FILE__, __LINE__, __func__)
 static inline int nf7util_log_uv_assert_(
     int ret, const char* file, uint64_t line, const char* func) {
   nf7util_log_uv_(ret, file, line, func);
